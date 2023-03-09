3663f1318067e858d0627e88bf01d09b7d6a1c5f
Beniamin Muresan <beniamin.muresan@fabelx.com>
2023-03-09 16:20:21 +0200

    Fix Keycloack user id print

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,7 +1,9 @@
 class AuthenticatedController < ApplicationController
   def user
     keycloak_id = Keycloak::Helper.current_user_id(request.env)
+    puts "------ Keycloak USER ID ------"
     puts keycloak_id
+    puts "------ Keycloak USER ID ------"
     User.find_by(keycloak_id: keycloak_id)
   end
 end
